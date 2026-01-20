1848c826b392a10805c54bb4f6b67757373cd6d4
Gonçalo Vieira <123480612+Goncas777@users.noreply.github.com>
2026-01-20 16:53:48 +0000

    Delete terraform directory

--- terraform/providers.tf
+++
@@ -1,8 +0,0 @@
-# Default Kubernetes provider
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = var.kube_context
-}
-
-# Configure TLS provider (no iteration needed - one instance)
-provider "tls" {}
